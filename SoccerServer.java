251808f38db8990e18b87c65f1d76c3ace355a28
Jiakuan <jiak94@uga.edu>
2015-08-25 09:56:26 -0400

    server.java

--- SoccerServer.java
+++ SoccerServer.java
@@ -2,4 +2,23 @@
  * Created by Jiakuan on 8/24/2015.
  */
 public class SoccerServer {
+    public static final int MAX_X = 80;
+    public static final int MAX_Y = 23;
+    public static final int TIME_LIMIT = 20;
+    public static final int TIME_OUT = 3000;
+    public static final int KICK_DIST = 10;
+
+    private int ball_x;
+    private int ball_y;
+    private int[] player_x = new int[8];
+    private int[] player_y = new int[8];
+
+    private int[][] field = new int[MAX_X][MAX_Y];
+
+    private boolean display = true;
+    private int points = 7;
+
+    public void reportScore(int west_score, int east_score) {
+
+    }
 }
